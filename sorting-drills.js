dfbfb79115d98afd291829692a2ee9848e45f07d
nicknotj <nicknotjustin@gmail.com>
2019-03-07 11:23:21 -0600

    qSort now counts the number of steps and returns as part of returned obj.

--- sorting-drills.js
+++ sorting-drills.js
@@ -1,18 +1,27 @@
 'use strict';
 
 const datastring = '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
+//const datastring = '2 3 1';
+
 const dataset = datastring.split(' ').map(num => Number(num));
 
 // Quick sort
-const qSort = (array, start = 0, end = array.length, steps = 1) => {
+const qSort = (array, start = 0, end = array.length, steps = 0) => {
+  steps++
+  // console.log('qSort Ran!', steps);
+  const obj = {array, steps};
   if (start >= end) {
-    return array;
+    return obj;
   }
 
   const middle = partition(array, start, end);
-  array = qSort(array, start, middle, steps++);
-  array = qSort(array, middle + 1, end, steps++);
-  return array;
+  let left = qSort(array, start, middle, steps);
+  obj.array = left.array;
+  obj.steps = left.steps;
+  let right = qSort(array, middle + 1, end, obj.steps);
+  obj.array = right.array;
+  obj.steps = right.steps;
+  return obj;
 };
 
 const partition = (array, start, end) => {
